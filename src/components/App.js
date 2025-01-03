ee99be219602bef86a46c1230ef0b551d0292909
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2025-01-03 19:50:48 +0000

    regenerator install

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 // import { error } from "cypress/types/jquery";
 
 const App = () => {
   const [data, setData] = useState(null); // Initialize state with null
 
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then(response => response.json()) // Parse JSON data
-      .then(data => setData(data)) // Update state with fetched data
-      .catch(error => setData(error)); // Log errors
-    // const fetchData = async () => {
-    //   try {
-    //     const response = await fetch("https://dummyjson.com/products"); // Fetch data
-    //     if(!response.ok){
-    //       throw new Error(`HTTP error! status: ${response.status}`);
-    //     }
-    //     const result = await response.json(); // Parse JSON data
-    //     setData(result); // Update state with fetched data
-    //   } catch (error) {
-    //     setData(error.mess) // Log errors
-    //   }
-    // };
+    // fetch("https://dummyjson.com/products")
+    //   .then(response => response.json()) // Parse JSON data
+    //   .then(data => setData(data)) // Update state with fetched data
+    //   .catch(error => setData(error)); // Log errors
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products"); // Fetch data
+        if(!response.ok){
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const result = await response.json(); // Parse JSON data
+        setData(result); // Update state with fetched data
+      } catch (error) {
+        setData(error.mess) // Log errors
+      }
+    };
 
-    // fetchData(); 
+    fetchData(); 
   }, []); // Empty dependency array ensures this runs once on component mount
 
   return (
